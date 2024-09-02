3986dffc412d8df9e891ab8e4fb492ec91bcd2f3
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-09-03 01:56:42 +0530

    Refactor: ui uupdate

--- src/app/components/Favorites.tsx
+++ src/app/components/Favorites.tsx
@@ -13,7 +13,7 @@ const Favorites: React.FC = () => {
   );
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {favoriteProperties.length > 0 ? (
         favoriteProperties.map((property) => (
           <PropertyCard key={property.id} property={property} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function HomePage() {
   };
 
   return (
-    <div className="container mx-auto p-4 min-h-[80vh] ">
+    <div className="container mx-auto p-4  ">
       {/* <h1 className="text-4xl font-bold text-center mb-8">Property Listings</h1> */}
       <Hero/>
       <FilterBar onFilterChange={handleFilterChange} />
