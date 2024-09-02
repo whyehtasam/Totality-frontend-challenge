e67e75233c145de3d31aea1b060c8e271c8992b4
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-09-02 16:29:09 +0530

    Refactor: added particles component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,24 @@ import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
+import Particles from "@/components/magicui/particles";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { theme } = useTheme();
+  const [color, setColor] = useState("#ffffff");
+
+  useEffect(() => {
+    setColor(theme === "dark" ? "#ffffff" : "#000000");
+  }, [theme]);
   return (
     <html lang="en">
-      <body className="bg-slate-50 dark:bg-slate-950">
+      <body className="bg-slate-50 dark:bg-slate-950 relative bg-background">
         <BookingProvider>
           <ThemeProvider
             attribute="class"
@@ -24,6 +33,13 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <Navbar />
+            <Particles
+              className="absolute inset-0 -z-10 h-full"
+              quantity={200}
+              ease={180}
+              color={color}
+              refresh
+            />
             <main>{children}</main>
             <Footer className="" />
           </ThemeProvider>
